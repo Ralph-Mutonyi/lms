ba84a11c3361293935c6f3d07f1d617f35930c1b
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-04-27 00:28:18 +0300

    First commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Ralphmutonyi#32',
     database: 'learning_management'
 });
 
